bdbf58a307cfe829813a96befc713fbd5fbec348
mostafaramadann <mo_star44@hotmail.com>
2023-04-02 00:32:01 +0200

    mlp

---
+++ mvc/model/mlp.cpp
@@ -0,0 +1,22 @@
+//
+// Created by mostafa on 4/2/23.
+//
+#include "mlp.h"
+
+auto frand = [](){return (2*(double)rand()/RAND_MAX)-1.0;};
+
+Perceptron::Perceptron(int inputs, double bias) {
+    this->bias = bias;
+    this->weights.resize(inputs+1);
+    std::generate(weights.begin(),weights.end(),frand);
+
+}
+double Perceptron::sigmoid(double x){
+    return  1/(1+ exp(-x));
+}
+
+double Perceptron::run(std::vector<double> x) {
+    x.push_back(bias);
+    double sum = inner_product(x.begin(),x.end(),weights.begin(),(double)0.0);
+    return sigmoid(sum);
+}
---
+++ mvc/model/mlp.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <algorithm>
+#include <ctime>
+#include <numeric>
+class Perceptron{
+public:
+    std::vector<double> weights;
+    double bias;
+    Perceptron(int inputs, double bias=1.0);
+    double run(std::vector<double>);
+    void set_weights(std::vector<double>w_init);
+    double sigmoid(double);
+};
